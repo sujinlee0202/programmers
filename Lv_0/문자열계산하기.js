d1dcef637debe292b9c3fc4b547b516578a31590
sujinlee0202 <sujinlee0202@gmail.com>
2022-11-07 13:27:42 +0900

    Level 0
    문자열 계산하기

--- Lv_0/문자열계산하기.js
+++ Lv_0/문자열계산하기.js
@@ -1,22 +1,20 @@
-// const solution = (str) => {
-//   let answer = 0;
+const solution = (str) => {
+  let answer = 0;
 
-//   const arr = str.split(' ')
+  const arr = str.split(' ')
 
-//   for(let i=0; i<arr.length; i++) {
-//     if(i%2 !== 0 && arr[i] === '-') {
-//       arr[i+1] = arr[i+1] * -1
-//     }
+  for(let i=0; i<arr.length; i++) {
+    if(i%2 !== 0 && arr[i] === '-') {
+      arr[i+1] = arr[i+1] * -1
+    }
 
-//     if(i%2 === 0) {
-//       answer = answer + Number(arr[i])
-//     }
-//   }
+    if(i%2 === 0) {
+      answer = answer + Number(arr[i])
+    }
+  }
 
-//   return answer;
-// }
-
-var solution=eval
+  return answer;
+}
 
 console.log(solution("3 + 4"))
 

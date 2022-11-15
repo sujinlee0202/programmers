9585227225007e9dbc4757a9a5513bdf9b089efa
sujinlee0202 <sujinlee0202@gmail.com>
2022-11-15 18:24:24 +0900

    Level 1 추가
    문자열을 정수로 바꾸기

---
+++ Lv_1/문자열을정수로바꾸기.js
@@ -0,0 +1,12 @@
+const solution = (str) => {
+  let answer;
+
+  answer = Number(str)
+  console.log(answer, typeof(answer))
+
+  return answer;
+}
+
+
+let str = '-1234'
+console.log(solution(str))
